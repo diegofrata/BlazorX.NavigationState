63a1d5be6c692b0c73bc308e5df8da021f776563
Diego Frata <d@frata.io>
2020-08-12 18:55:02 +0100

    Fix DependencyInjection bug.

--- BlazorX.NavigationState/DependencyInjection.cs
+++ BlazorX.NavigationState/DependencyInjection.cs
@@ -4,7 +4,7 @@ namespace BlazorX.NavigationState
 {
     public static class DependencyInjection
     {
-        public static void AddNavigationState(this ServiceCollection collection)
+        public static void AddNavigationState(this IServiceCollection collection)
         {
             collection.AddScoped<INavigationState, NavigationState>();
         }
